a6ad4cc96c5ea8f14350bfc52d71485ae4b160e5
Ian Roy <ian.t.roy@gmail.com>
2016-03-16 16:31:42 -0400

    CM-13 bringup
    Change-Id: If1d14808bf6e7c2e78add6b48f6328e393056537

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -2698,7 +2698,7 @@ static int __mem_cgroup_try_charge(struct mm_struct *mm,
 	 * in system level. So, allow to go ahead dying process in addition to
 	 * MEMDIE process.
 	 */
-	if (unlikely(test_thread_flag(TIF_MEMDIE)
+	if (unlikely(test_thread_flag_relaxed(TIF_MEMDIE)
 		     || fatal_signal_pending(current)))
 		goto bypass;
 
@@ -4095,7 +4095,7 @@ static void mem_cgroup_do_uncharge(struct mem_cgroup *memcg,
 	 * because we want to do uncharge as soon as possible.
 	 */
 
-	if (!batch->do_batch || test_thread_flag(TIF_MEMDIE))
+	if (!batch->do_batch || test_thread_flag_relaxed(TIF_MEMDIE))
 		goto direct_uncharge;
 
 	if (nr_pages > 1)
@@ -5790,16 +5790,17 @@ static void mem_cgroup_usage_unregister_event(struct cgroup *cgrp,
 swap_buffers:
 	/* Swap primary and spare array */
 	thresholds->spare = thresholds->primary;
-	/* If all events are unregistered, free the spare array */
-	if (!new) {
-		kfree(thresholds->spare);
-		thresholds->spare = NULL;
-	}
 
 	rcu_assign_pointer(thresholds->primary, new);
 
 	/* To be sure that nobody uses thresholds */
 	synchronize_rcu();
+
+	/* If all events are unregistered, free the spare array */
+	if (!new) {
+		kfree(thresholds->spare);
+		thresholds->spare = NULL;
+	}
 unlock:
 	mutex_unlock(&memcg->thresholds_lock);
 }
